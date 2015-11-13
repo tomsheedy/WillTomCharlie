d006c4e37c310c9be464f00de2e3fb6492b79e25
a2-painter <a2-painter@STU-CZC1295Y9J.campus.ads.uwe.ac.uk>
2015-11-13 15:21:57 +0000

    LoginBean

--- ESD/src/java/Controllers/LoginController.java
+++ ESD/src/java/Controllers/LoginController.java
@@ -5,28 +5,30 @@
  */
 package Controllers;
 
+import Models.LoginBean;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-
 
 /**
  *
  * @author a2-painter
  */
 public class LoginController extends HttpServlet {
-    
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse
-            response) throws ServletException, IOException{
-        
-        
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String name = request.getParameter("name");
         String password = request.getParameter("password");
+        RequestDispatcher rd = null;
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        LoginBean login = new LoginBean(name, password);
+        request.setAttribute("LoginBean", login);
         
         out.println("<html>");
         out.println("<body>");
@@ -34,7 +36,7 @@ public class LoginController extends HttpServlet {
         out.println("<>");
         out.println("</body>");
         out.println("</html>");
-        
+
         out.close();
     }
 }
